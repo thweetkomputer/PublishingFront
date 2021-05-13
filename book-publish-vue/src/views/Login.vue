ad53938e57b70d65533b7f1722ea4ec2f6b9202e
thweetkomputer <2320092610@qq.com>
2021-05-13 10:45:14 +0800

    修改了清空异常的问题

--- book-publish-vue/src/views/Login.vue
+++ book-publish-vue/src/views/Login.vue
@@ -5,126 +5,72 @@
       <!--        <img class="mlogo" src="http://o.bookschina.com/images/logo0508.png" alt="">-->
       <!--      </el-header>-->
 
-<!--      <el-main>-->
-        <el-tabs type="border-card" style="width: 340px; margin:100px auto auto;">
-          <el-tab-pane>
-            <div slot="label" style="width: 128px;">登录</div>
-            <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px"
-                     class="demo-ruleForm">
-              <el-form-item label="用户名" prop="username">
-                <el-input maxlength="16" placeholder="请输入用户名" v-model="ruleForm.username" clearable
-                          prefix-icon="el-icon-user-solid"
-                          style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>
-              </el-form-item>
-              <el-form-item label="密 码" prop="password">
-                <el-input maxlength="19" placeholder="请输入密码" type="password" v-model="ruleForm.password" show-password
-                          clearable prefix-icon="el-icon-s-goods"
-                          style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>
-              </el-form-item>
-              <el-form-item>
-                <el-row style="text-align: center; margin-top: -10px;;">
-                  <el-checkbox v-model="remember_me">记住我</el-checkbox>
-                  <el-divider direction="vertical"></el-divider>
-                  <el-link :underline="false">忘记密码？</el-link>
-                  <!--                <el-link type="primary">g</el-link>-->
-                </el-row>
-              </el-form-item>
-              <el-form-item>
-                <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
-                <el-button @click="resetForm('ruleForm')">重置</el-button>
-              </el-form-item>
-            </el-form>
-          </el-tab-pane>
-          <el-tab-pane>
-            <div slot="label" style="width: 128px">注册</div>
-            <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px"
-                     class="demo-ruleForm">
-              <el-form-item label="用户名" prop="username_signup">
-                <el-input maxlength="16" placeholder="请输入用户名" v-model="ruleForm.username_signup" clearable
-                          prefix-icon="el-icon-user-solid"
-                          style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>
-              </el-form-item>
-              <el-form-item label="密  码" prop="pass">
-                <el-input maxlength="19" placeholder="请输入密码" type="password" v-model="ruleForm.pass" autocomplete="off"
-                          show-password
-                          clearable prefix-icon="el-icon-goods"
-                          style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>
-              </el-form-item>
-              <el-form-item label="确认密码" prop="checkPass">
-                <el-input maxlength="19" placeholder="请输入确认密码" type="password"
-                          v-model="ruleForm.checkPass" autocomplete="off"
-                          show-password clearable
-                          prefix-icon="el-icon-s-goods"
-                          style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>
-              </el-form-item>
-              <el-form-item label="邮  箱" prop="email">
-                <el-input maxlength="321" placeholder="请输入邮箱" type="email" v-model="ruleForm.email" clearable
-                          prefix-icon="el-icon-message"
-                          style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>
-              </el-form-item>
-              <el-form-item>
-                <el-button type="primary" @click="submitForm('ruleForm')">立即注册</el-button>
-                <el-button @click="resetForm('ruleForm')">重置</el-button>
-              </el-form-item>
-            </el-form>
-          </el-tab-pane>
-        </el-tabs>
-        <!--        <el-form v-show="isLogin" :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="60px" class="demo-ruleForm">-->
-        <!--          <el-form-item label="用户名" prop="username">-->
-        <!--            <el-input maxlength="16" placeholder="请输入用户名" v-model="ruleForm.username" clearable-->
-        <!--                      prefix-icon="el-icon-user-solid"-->
-        <!--                      style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>-->
-        <!--          </el-form-item>-->
-        <!--          <el-form-item label="密 码" prop="password">-->
-        <!--            <el-input maxlength="19" placeholder="请输入密码" type="password" v-model="ruleForm.password" show-password-->
-        <!--                      clearable prefix-icon="el-icon-s-goods"-->
-        <!--                      style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>-->
-        <!--          </el-form-item>-->
-        <!--&lt;!&ndash;          <a href="#">忘记密码</a>&ndash;&gt;-->
-        <!--          <el-form-item style="">-->
-        <!--              <el-row style="text-align: center; margin-top: -10px;;">-->
-        <!--                <el-checkbox v-model="remember_me">记住我</el-checkbox>-->
-        <!--                <el-link :underline="false">忘记密码</el-link>-->
-        <!--&lt;!&ndash;                <el-link type="primary">g</el-link>&ndash;&gt;-->
-        <!--              </el-row>-->
-        <!--          </el-form-item>-->
-        <!--          <el-form-item>-->
-        <!--            <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>-->
-        <!--            <el-button @click="resetForm('ruleForm')">重置</el-button>-->
-        <!--          </el-form-item>-->
-
-        <!--        </el-form>-->
-
-        <!--        <el-form v-show="!isLogin" :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">-->
-        <!--          <el-form-item label="用户名" prop="username" >-->
-        <!--            <el-input maxlength="16" placeholder="请输入用户名" v-model="ruleForm.username" clearable-->
-        <!--                      prefix-icon="el-icon-user-solid"-->
-        <!--                      style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>-->
-        <!--          </el-form-item>-->
-        <!--          <el-form-item label="密  码" prop="pass">-->
-        <!--            <el-input maxlength="19" placeholder="请输入密码" type="password" v-model="ruleForm.pass" autocomplete="off"-->
-        <!--                      show-password-->
-        <!--                      clearable prefix-icon="el-icon-goods"-->
-        <!--                      style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>-->
-        <!--          </el-form-item>-->
-        <!--          <el-form-item label="确认密码" prop="checkPass">-->
-        <!--            <el-input maxlength="19" placeholder="请输入确认密码" type="password"-->
-        <!--                      v-model="ruleForm.checkPass" autocomplete="off"-->
-        <!--                      show-password clearable-->
-        <!--                      prefix-icon="el-icon-s-goods"-->
-        <!--                      style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>-->
-        <!--          </el-form-item>-->
-        <!--          <el-form-item label="邮  箱" prop="email">-->
-        <!--            <el-input maxlength="321" placeholder="请输入邮箱" type="email" v-model="ruleForm.email" clearable-->
-        <!--                      prefix-icon="el-icon-message"-->
-        <!--                      style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>-->
-        <!--          </el-form-item>-->
-        <!--          <el-form-item>-->
-        <!--            <el-button type="primary" @click="submitForm('ruleForm')">立即注册</el-button>-->
-        <!--            <el-button @click="resetForm('ruleForm')">重置</el-button>-->
-        <!--          </el-form-item>-->
-        <!--        </el-form>-->
-<!--      </el-main>-->
+      <!--      <el-main>-->
+      <el-tabs type="border-card" style="width: 340px; margin:100px auto auto;">
+        <el-tab-pane >
+          <div slot="label" style="width: 128px;">登录</div>
+          <el-form :model="ruleFormLogin" status-icon :rules="rulesLogin" ref="ruleFormLogin" label-width="100px"
+                   class="demo-ruleForm">
+            <el-form-item label="用户名" prop="username">
+              <el-input maxlength="16" placeholder="请输入用户名" v-model="ruleFormLogin.username" clearable
+                        prefix-icon="el-icon-user-solid"
+                        style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>
+            </el-form-item>
+            <el-form-item label="密 码" prop="password">
+              <el-input maxlength="19" placeholder="请输入密码" type="password" v-model="ruleFormLogin.password"
+                        show-password
+                        clearable prefix-icon="el-icon-s-goods"
+                        style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-row style="text-align: center; margin-top: -10px;;">
+                <el-checkbox v-model="remember_me">记住我</el-checkbox>
+                <el-divider direction="vertical"></el-divider>
+                <el-link :underline="false">忘记密码？</el-link>
+                <!--                <el-link type="primary">g</el-link>-->
+              </el-row>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitFormLogin('ruleFormLogin')">登录</el-button>
+              <el-button @click="resetForm('ruleFormLogin')">重置</el-button>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+        <el-tab-pane>
+          <div slot="label" style="width: 128px">注册</div>
+          <el-form :model="ruleFormSignup" status-icon :rules="rulesSignup" ref="ruleFormSignup" label-width="100px"
+                   class="demo-ruleForm">
+            <el-form-item label="用户名" prop="username">
+              <el-input maxlength="16" placeholder="请输入用户名" v-model="ruleFormSignup.username" clearable
+                        prefix-icon="el-icon-user-solid"
+                        style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>
+            </el-form-item>
+            <el-form-item label="密  码" prop="pass">
+              <el-input maxlength="19" placeholder="请输入密码" type="password" v-model="ruleFormSignup.pass"
+                        autocomplete="off"
+                        show-password
+                        clearable prefix-icon="el-icon-goods"
+                        style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>
+            </el-form-item>
+            <el-form-item label="确认密码" prop="checkPass">
+              <el-input maxlength="19" placeholder="请输入确认密码" type="password"
+                        v-model="ruleFormSignup.checkPass" autocomplete="off"
+                        show-password clearable
+                        prefix-icon="el-icon-s-goods"
+                        style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>
+            </el-form-item>
+            <el-form-item label="邮  箱" prop="email">
+              <el-input maxlength="321" placeholder="请输入邮箱" type="email" v-model="ruleFormSignup.email" clearable
+                        prefix-icon="el-icon-message"
+                        style="box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitFormSignup('ruleFormSignup')">立即注册</el-button>
+              <el-button @click="resetForm('ruleFormSignup')">重置</el-button>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+      </el-tabs>
     </el-container>
   </div>
 </template>
@@ -134,7 +80,9 @@ export default {
   name: "Login",
   data() {
     const checkUsername = (rule, value, callback) => {
+      console.log("checkUsername")
       if (value === '') {
+        console.log('value')
         return callback(new Error('用户名不能为空'))
       } else if (value.toString().length > 15) {
         return callback(new Error('用户名长度不超过15'))
@@ -152,6 +100,7 @@ export default {
       }
     };
     const checkUsername2 = (rule, value, callback) => {
+      console.log("checkUsername2:", value)
       if (value === '') {
         return callback(new Error('用户名不能为空'))
       } else if (value.toString().length > 15) {
@@ -182,8 +131,8 @@ export default {
       } else if ((value + "").toString().length > 18 || (value + "").toString().length < 6) {
         callback(new Error('密码长度为6-18位'));
       } else {
-        if (this.ruleForm.checkPass !== '') {
-          this.$refs.ruleForm.validateField('checkPass');
+        if (this.ruleFormSignup.checkPass !== '') {
+          this.$refs.ruleFormSignup.validateField('checkPass');
         }
         callback();
       }
@@ -191,29 +140,25 @@ export default {
     const validatePass2 = (rule, value, callback) => {
       if (value === '') {
         callback(new Error('确认密码不能为空'));
-      } else if (value !== this.ruleForm.pass) {
+      } else if (value !== this.ruleFormSignup.pass) {
         callback(new Error('两次输入密码不一致!'));
       } else {
         callback();
       }
     };
     return {
-      ruleForm: {
-        username: '',
-        password: '',
-        username_signup: '',
-        pass: '',
-        checkPass: '',
-        email: ''
-      },
-      rules: {
+      ruleFormLogin: {},
+      ruleFormSignup: {},
+      rulesLogin: {
         username: [
           {validator: checkUsername, trigger: 'change'},
         ],
         password: [
           {validator: validatePass, trigger: 'blur'}
-        ],
-        username_signup: [
+        ]
+      },
+      rulesSignup: {
+        username: [
           {validator: checkUsername2, trigger: 'change'},
         ],
         pass: [
@@ -223,23 +168,23 @@ export default {
           {validator: validatePass2, trigger: 'blur'},
         ],
         email: [
-          {trigger: 'change', validator: checkEmail}
+          {validator: checkEmail, trigger: 'change'},
         ],
       },
       isLogin: true,
-      remember_me: false
+      remember_me: false,
     };
   },
   created() {
     this.isLogin = true
   },
   methods: {
-    submitForm(formName) {
+    submitFormLogin(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
           // alert('submit!');
           const _this = this
-          this.$axios.post('/login', this.ruleForm).then(res => {
+          this.$axios.post('/login', this.ruleFormLogin).then(res => {
             const jwt = res.headers['authorization']
             const userInfo = res.data.data
             console.log(userInfo)
@@ -258,6 +203,36 @@ export default {
             })
 
             this.$router.push('/books')
+          })
+        } else {
+          console.log('error submit!!!');
+          return false;
+        }
+      });
+    },
+    submitFormSignup(formName) {
+      console.log('signup')
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          // alert('submit!');
+          const _this = this
+          this.$axios.post('/signup', this.ruleFormSignup).then(res => {
+            const jwt = res.headers['authorization']
+            const userInfo = res.data.data
+            console.log(userInfo)
+
+            // Share the data
+            _this.$store.commit('SET_TOKEN', jwt)
+            _this.$store.commit('SET_USERINFO', userInfo)
+
+            console.log(_this.$store.getters.getUser)
+            this.$message({
+              showClose: true,
+              message: '注册成功，请重新登录',
+              type: 'success',
+              duration: 2000
+            })
+            _this.$router.go(0)
           })
         } else {
           console.log('error submit!!');

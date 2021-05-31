f8f86593c2e5c4200b703f052e9eb51ebbe9cdbb
thweetkomputer <2320092610@qq.com>
2021-05-31 15:00:59 +0800

    消除分页bug

--- book-publish-vue/src/views/Article.vue
+++ book-publish-vue/src/views/Article.vue
@@ -80,12 +80,12 @@ export default {
         },
       }).then((res) => {
         this.article_list = res.data.data.article_list;
-        this.total_num=res.data.data.tatal_num;
+        this.total_num=res.data.data.total_num;
         if(this.total_num%10!=0){
-          this.total=this.total.num/10+1;
+          this.total=this.total_num+1;
         }
         else{
-          this.total=this.total.num/10;
+          this.total=this.total_num;
         }
         console.log(this.total)
       });

fbccc8edfe9132a8f385539b5b79f7f17df0c5a7
thweetkomputer <2320092610@qq.com>
2021-06-09 09:53:31 +0800

    消息中心模块完成

--- book-publish-vue/src/views/Message.vue
+++ book-publish-vue/src/views/Message.vue
@@ -12,15 +12,21 @@
         <el-col v-for="item in message" :key="item.id" :span="24">
           <div class="card dewb" >
             <el-row>
-              <el-col :xs="24" :lg="18" v-show="item.has_read===400" @click="open(item.id)">
-                <span style="float: left">该消息已读</span>
-                <span style="float: right">{{item.created_time}}</span>
-                <div> {{ item.content }} </div>
+              <el-col :xs="24" :lg="18" v-show="item.hasRead===1" >
+                <div @click="open(item.id)">
+                  <span style="float: left">该消息已读 </span>
+                  <span style="float: right">{{item.createdTime}}</span>
+                  <div> {{ item.content }} </div>
+                </div>
+
               </el-col>
-              <el-col :xs="24" :lg="18" v-show="item.has_read!==400" @click="open(item.id)">
-                <span style="float: left">您有一条新通知</span>
-                <span style="float: right">{{item.created_time}}</span>
-                <div> {{ item.content}} </div>
+              <el-col :xs="24" :lg="18" v-show="item.hasRead!==1" >
+                <div @click="open(item.id)">
+                  <span style="float: left">您有一条新通知 </span>
+                  <span style="float: right">{{item.createdTime}}</span>
+                  <div> {{ item.content}} </div>
+                </div>
+
               </el-col>
             </el-row>
           </div>
@@ -59,6 +65,17 @@ export default {
   },
   methods: {
     open(id) {
+      for (let i = 0; i < this.message.length; i++) {
+        // console.log(this.message[i].id)
+        if (this.message[i].id === id) {
+          // alert(this.message[i].content)
+          this.$alert(this.message[i].content,  "消息内容",{
+            confirmButtonText: '确定',
+            callback: action => {
+            }
+          });
+        }
+      }
       axios({
         url: "/readNotice",
         method: "get",
@@ -66,12 +83,20 @@ export default {
           notice_id:id
         },
       }).then((res) => {
-      });
-      this.$alert(this.notice.content, this.notice.title, {
-        confirmButtonText: '确定',
-        callback: action => {
+        // for (let m in  this.message) {
+        //   if (m.id === id) {
+        //     m.hasRead = 1
+        //   }
+        // }
+        for (let i = 0; i < this.message.length; i++) {
+          // console.log(this.message[i].id)
+          if (this.message[i].id === id) {
+            // alert(this.message[i].content)
+            this.message[i].hasRead = 1
+          }
         }
       });
+
     },
     //跳转内容页
     getMessageData(page) {

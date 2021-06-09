2a28cea1135656e3bba43746561b8e116f71ada8
buaaer-stack <2550806098@qq.com>
2021-06-09 19:38:27 +0800

    1

--- book-publish-vue/src/views/Homepage.vue
+++ book-publish-vue/src/views/Homepage.vue
@@ -1,140 +1,235 @@
-<!-- 用户中心 -->
 <template>
-  <div>
-    <div style="width:50%;float: left">
-      <el-card class="box-card">
-        <div slot="header" class="clearfix">
-          <span>基本信息</span>
-        </div>
-        <div class="card">
-          <span>用户名</span>
-        </div>
-        <div class="card">
-          <span>个人邮箱</span>
-        </div>
-        <div class="card">
-          <span>我的文章数</span>
-        </div>
-        <div class="card">
-          <span>收藏文章数</span>
-        </div>
-        <div>
-          psan
-          <el-input placeholder="请输入密码" v-model="input" show-password></el-input>
-        </div>
-      </el-card>
+  <div id="Books">
+    <div>
+      <el-row>
+        <el-col  :span="24" style="margin-bottom: 3px;">
+          <h3 style="margin-left: 100px">用户名：{{}}</h3>
+          <div style="margin-top: 10px;margin-left: 100px">身份：{{}}</div>
+          <div style="margin-top: 10px;margin-left: 100px">邮箱：{{}}</div>
+
+          <div style="margin-top: 10px;margin-left: 100px;">
+            个人简介
+            <span style="float: right;margin-right: 460px;margin-top:-25px"><el-button @click="modify()" style="margin-top: 10px">修改个人简介</el-button></span>
+          </div>
+          <div v-show="isopen" >
+            <el-input v-model="description" placeholder="请输入内容" style="margin-top: 10px;margin-left: 100px" type="textarea"
+                      :maxlength="120"
+                      :rows="2"></el-input>
+            <el-button @click="submit" style="margin-top: 10px;margin-left: 100px">确定</el-button>
+            <el-button @click="consoleSubmit" style="margin-top: 10px;margin-left: 100px">取消</el-button>
+          </div>
+          <div v-show="!isopen" style="margin-left: 100px">
+            {{}}
+          </div>
+
+        </el-col>
+      </el-row>
     </div>
-    <div style="width: 48%;float: right;">
-<!--      <el-calendar v-model="value" style="background-color: #B3C0D1">-->
-<!--      </el-calendar>-->
+    <div id="content" style="margin-left: 0px;margin-top: -70px">
+      <h5>我的动态</h5>
+      <div class="article" style="color: #00000060;width: 60%;float: left">
+        <el-row class="fudong">
+          <el-col v-for="item in article_list" :key="item.id" :span="24" style="margin-bottom: 3px;">
+            <div class="card dewb" style="height: 120px" @click="toArticle(item.id)">
+              <el-row>
+                <el-col :xs="24" :lg="18">
+                  <div style="padding-left: 30px; padding-top: 10px">
+                    <div class="word"><h2 style="display: inline">{{ item.title }}</h2><span style="padding-left: 10px; padding-top: 15px; float: right">{{item.type}}</span></div>
+                    <hr>
+                    <div class="word"> {{ item.description }}</div>
+                  </div>
+                </el-col>
+              </el-row>
+            </div>
+          </el-col>
+        </el-row>
+        <el-pagination
+            background
+            :hide-on-single-page="value"
+            layout="prev, pager, next"
+            :total="total"
+            :page-size="pageSize"
+            @current-change="currentChange"
+        >
+        </el-pagination>
+      </div>
+      <div class="side" style="width: 39%;margin-left: 888px; position: fixed">
+        <el-row>
+          <el-col :xs="10" :lg="10">
+            <div style=" ">
+              <el-carousel :interval="4000" height="240px" style="float: left;width:290px;border-radius: .25rem;">
+                <el-carousel-item v-for="item in imagebox" :key="item.id">
+                  <img :src="item.idView" class="image">
+                </el-carousel-item>
+              </el-carousel>
+            </div>
+          </el-col>
+          <el-col :xs='10' :lg="10" style="float: left;margin-top: 245px;position: absolute">
+            <div class=""
+                 style="background-color:#fff; height:240px;width: 290px; box-shadow: 0 2px 4px rgba(0,0,0,.12),0 0 6px rgba(0,0,0,.04);border-radius: .25rem;">
+              <h5 style="color:black; padding-top: 7px; padding-left: 5px">热门标签</h5>
+              <hr>
+              <el-tag type="info" class="tag">历史</el-tag>
+              <el-tag type="info" class="tag">科技</el-tag>
+              <br>
+              <el-tag type="info" class="tag">人文</el-tag>
+              <el-tag type="info" class="tag">军事</el-tag>
+              <el-tag type="info" class="tag">地理</el-tag>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+
     </div>
   </div>
 </template>
-<script src="../assets/css/sex/iconfont.js">
+
+<script>
 import axios from "axios";
+
 export default {
+  name: "books",
   data() {
     return {
-      circleUrl: "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
-      input: '',
-      userInfo: {},
-      textarea: JSON.parse(this.$store.state.userInfo).description,
-      value: new Date()
+      imagebox: [{id: 0, idView: require('../assets/img/img1.jpg')},
+        {id: 1, idView: require('../assets/img/img2.jpg')},
+        {id: 2, idView: require('../assets/img/img4.png')},
+
+        //imagebox是assets下一个放图片的文件夹
+      ],
+      description:'',
+      article_list: {},
+      currentPage: 1,
+      pageSize: 10,
+      total: 10,
+      //total是条目总数，
+      total_num: 0,
+      value: false,
+      isopen:false,
+      userInfo:JSON.parse(this.$store.state.userInfo),
     }
   },
   mounted() {
-    this.getUserInfo();
+    this.getListData(this.currentPage);
   },
   methods: {
-    getUserInfo() {
-      this.userInfo = store.state.userInfo
+    consoleSubmit(){
+      this.isopen=false;
+      this.description='';
     },
-    submitInfo() {//保存编辑的用户信息
+    submit(){
+      alert(this.description)
       axios({
-        url: "/editInfo",
-        method: "post",
+        url: "",
+        method: "get",
         params: {
-          user_id: JSON.parse(this.$store.state.userInfo).id,
-          description: this.textarea
+          user_id:this.userInfo.id,
+          description:this.description
         },
       }).then((res) => {
-        const userInfo = res.data.data
-        console.log(userInfo)
-        this.$store.commit('SET_USERINFO', userInfo)
-        console.log(this.$store.getters.getUser)
-        this.$router.push("/Homepage")
+        this.isopen=false;
+        this.description='';
       });
+    },
+    modify(){
+      this.isopen=true;
+    },
+    //跳转内容页
+    toArticle(id) {
+      let routeUrl = this.$router.resolve({
+        path: "/content",
+        query: {id: id}
+      });
+      window.open(routeUrl.href, '_blank');
+    },
+    getListData(page) {
+      axios({
+        url: "",
+        method: "get",
+        params: {
+          page,
+          pageSize: this.pageSize,
+        },
+      }).then((res) => {
+        this.article_list = res.data.data.article_list;
+        this.total = res.data.data.total_num;
+        console.log(this.total)
+      });
+    },
+    currentChange(val) {
+      console.log("第" + val + "页");
+      this.currentPage = val;
+      this.getListData(val);
     },
   },
 }
 </script>
 
-<style>
-.el-button--primary {
-  color: #FFF;
-  background-color: #409EFF;
-  border-color: #409EFF;
+
+<style scoped>
+.el-row {
+  padding-bottom: 5px;
 }
 
-.el-button.is-circle {
-  border-radius: 50%;
-  padding: 10px;
-}
-
-.icon {
-  width: 26px;
-  height: 26px;
-  border-color: transparent;
-  background-color: transparent;
-}
-
-.el-input.is-disabled .el-input__inner {
-  background-color: white;
-  border-color: white;
-  color: black;
-  cursor: not-allowed;
-  height: 40px;
-}
-
-.text {
+.el-carousel__item h3 {
+  color: #475669;
   font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
 }
 
-.item {
-  margin-bottom: 25px;
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
 }
 
-.clearfix:before,
-.clearfix:after {
-  display: table;
-  content: "";
+.el-carousel__item:nth-child(2n+1) {
+  background-color: #d3dce6;
 }
 
-.clearfix:after {
-  clear: both
+.el-tag.el-tag--info {
+  margin: 5px;
 }
 
-.box-card {
-  width: 520px;
-}
-
-.box-card::shadow {
-  color: #696969;
-}
-
-.el-button--danger {
+.el-tag.el-tag--info {
+  background-color: #00000060;
+  border-color: #00000060;
   color: white;
-  background-color: transparent;
-  border-color: transparent;
 }
 
-.el-button--danger:hover {
-  color: #F56C6C;
-  background-color: transparent;
-  border-color: transparent;
+#article-list .dweb {
+  padding: 10px 10px;
 }
-.card{
-  margin-bottom:10px;
-  border:none;
+
+.card.dweb .text-item {
+  color: #fff;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.tag{
+  border-color: #d9d9d9!important;
+  background: #d9d9d9!important;
+  color: black!important;
+}
+
+
+.tag:hover{
+  color: #409EFF!important;
+
+}
+
+.fudong :hover {
+  background: #f7f7f7;
+  border-radius: 5px;
+}
+
+.word{
+  color: black;
+}
+.word:hover{
+  color: #265194!important;
 }
 </style>

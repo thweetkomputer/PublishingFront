85ca6de84477a7d2e2bc32c80abd9239c1e44076
thweetkomputer <2320092610@qq.com>
2021-06-10 15:39:25 +0800

    修改了Books的翻页器bug

--- book-publish-vue/src/views/Books.vue
+++ book-publish-vue/src/views/Books.vue
@@ -24,7 +24,7 @@
               background
               :hide-on-single-page="value"
               layout="prev, pager, next"
-              :total="total"
+              :total="total_num"
               :page-size="pageSize"
               @current-change="currentChange"
           >
